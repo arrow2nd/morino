1d190793e5febed6ecd085600bca7c155e874d47
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-18 20:24:27 +0900

    change: support for std@0.114.0

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 import "https://deno.land/x/dotenv@v3.1.0/load.ts";
-import { listenAndServe } from "https://deno.land/std@0.112.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import { getAsset, hasAsset } from "./lib/assets.js";
 import { getPage } from "./lib/page.js";
 
-function handleRequest(request) {
-  const { pathname, searchParams } = new URL(request.url);
-
+serve((req) => {
+  const { pathname, searchParams } = new URL(req.url);
+  
   if (hasAsset(pathname)) {
     return getAsset(pathname);
   }
-
+  
   return getPage(searchParams);
-}
-
-// TODO: std@0.114.0での変更に対応
-await listenAndServe(":8080", (req) => handleRequest(req));
+})
