fec0bfa34f1845ec26586b3028b8a1bb767e8baf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-11-18 10:48:53 +0900

    clean: JSDoc

--- assets/common.js
+++ assets/common.js
@@ -8,8 +8,8 @@ export const birth = {
 
 /**
  * 凛世さんのお誕生日までの秒数を計算
- * @param {Date} date Date
- * @returns 残り秒数
+ * @param {Date} date Dateオブジェクト
+ * @return {number} 残り秒数
  */
 export function calcSecond2Birthday(date) {
   const birthYear = (date.getMonth() > birth.mon ||
@@ -25,7 +25,7 @@ export function calcSecond2Birthday(date) {
  * テキストにデータを埋め込む
  * @param {string} text 対象の文字列
  * @param {Map<string, string>} data 埋め込むデータ
- * @returns 埋め込み後の文字列
+ * @return {string} 埋め込み後の文字列
  */
 export function embedData2Text(text, data) {
   data.forEach((val, key) => {
