0d6839d877908f3b4fc5c29b6e58f22077c89d42
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-31 10:50:59 +0900

    refactor: 依存関係を deps.js に集約

---
+++ deps.js
@@ -0,0 +1,4 @@
+export { serve } from "https://deno.land/std@0.114.0/http/server.ts";
+export { crypto } from "https://deno.land/std@0.114.0/crypto/mod.ts";
+export { encode } from "https://deno.land/std@0.114.0/encoding/base64url.ts";
+export * as esbuild from "https://deno.land/x/esbuild@v0.14.10/mod.js";
--- tools/build.js
+++ tools/build.js
@@ -1,4 +1,4 @@
-import * as esbuild from "https://deno.land/x/esbuild@v0.14.10/mod.js";
+import * as esbuild from "../deps.js";
 
 console.log("[Build Start]");
 
