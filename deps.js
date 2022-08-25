ca78caf5e2baf770994ba7a8f302600e8e0e8b35
arrow2nd <arrow2nd@users.noreply.github.com>
2022-08-25 15:09:48 +0000

    chore(deps): 依存関係を更新

--- deps.js
+++ deps.js
@@ -1,5 +1,5 @@
-export { serve } from "https://deno.land/std@0.152.0/http/server.ts";
-export { crypto } from "https://deno.land/std@0.152.0/crypto/mod.ts";
-export { encode } from "https://deno.land/std@0.152.0/encoding/base64url.ts";
-export { bundle } from "https://deno.land/x/emit@0.6.0/mod.ts";
+export { serve } from "https://deno.land/std@0.153.0/http/server.ts";
+export { crypto } from "https://deno.land/std@0.153.0/crypto/mod.ts";
+export { encode } from "https://deno.land/std@0.153.0/encoding/base64url.ts";
+export { bundle } from "https://deno.land/x/emit@0.7.0/mod.ts";
 export * as esbuild from "https://deno.land/x/esbuild@v0.14.10/mod.js";
