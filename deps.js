965f10a01d6d419a4585f269611a1424898da82d
denopendabot <denopendabot@github.com>
2023-10-21 00:05:51 +0000

    build(deps): bump esbuild from v0.19.4 to v0.19.5

--- deps.js
+++ deps.js
@@ -2,4 +2,4 @@ export { serve } from "https://deno.land/std@0.204.0/http/server.ts";
 export { crypto } from "https://deno.land/std@0.204.0/crypto/mod.ts";
 export { encode } from "https://deno.land/std@0.204.0/encoding/base64url.ts";
 export { bundle } from "https://deno.land/x/emit@0.30.0/mod.ts";
-export * as esbuild from "https://deno.land/x/esbuild@v0.19.4/mod.js";
+export * as esbuild from "https://deno.land/x/esbuild@v0.19.5/mod.js";
